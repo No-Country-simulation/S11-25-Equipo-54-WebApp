b61e0bd999f3fec69a3694363c5f8ffbdd3a9f0d
Alejandro <navarro.alejandro@outlook.es>
2025-12-09 15:46:54 -0600

    feat: add Category component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
 import "../../src/styles/globals.css";
+import Category from "./components/category/Category";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Category />
     </div>
   );
 }
